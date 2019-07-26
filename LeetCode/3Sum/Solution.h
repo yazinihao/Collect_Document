26cd26e9934ce359a58a29d3583d69aa94cafecb
柳衣 <sin8918@outlook.com>
2019-07-26 20:13:27 +0800

    Create Solution.h
    The sum of the three numbers is zero

---
+++ LeetCode/3Sum/Solution.h
@@ -0,0 +1,26 @@
+/*
+ * @Description: The sum of the three numbers is zero
+ * @Author: mr
+ * @Date: 2019-07-26 20:01:43
+ * @LastEditTime: 2019-07-26 20:12:21
+ * @LastEditors: Please set LastEditors
+ */
+#ifndef __3SUM_H
+#define __3SUM_H
+#include <vector>
+
+namespace Sum3Solution{
+    using namespace std;    
+    
+    class Solution
+    {
+    private:
+    public:
+        vector<vector<int>> threeSum(vector<int> nums)
+        {
+            
+        }
+    };
+}
+
+#endif
